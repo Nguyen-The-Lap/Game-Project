d40b62f1c1e570f38f428c4e53cf321166d83789
ThanhfVux2102 <24560017@gm.uit.edu.vn>
2025-03-31 01:33:05 +0700

    Update Shooting.cs
    update machine for weapon

--- PlayerAssets/Shooting.cs
+++ PlayerAssets/Shooting.cs
@@ -1,154 +1,152 @@
 using UnityEngine;
-using System;
-using System.Collections.Generic;
 using System.Collections;
 
 public class Weapon : MonoBehaviour
 {
-
-
+    [Header("References")]
     public Camera PlayerCamera;
+    public Animator weaponAnimator;
 
+    [Header("Ammo")]
+    public int maxAmmo = 30;
+    public int currentAmmo;
+    public int reserveAmmo = 90;
+    public float reloadTime = 2f;
+    private bool isReloading = false;
 
     [Header("Shooting")]
-    //Shooting
     public bool isShooting, readyToShoot;
-    bool allowReset = true;
-    public float shootingDelay = 2f;
-
+    private bool allowReset = true;
+    public float shootingDelay = 0.2f;
 
     [Header("Burst")]
     public int bulletsPerBurst = 3;
-    public int BurstBulletsLeft;
-
+    private int BurstBulletsLeft;
 
     [Header("Spread")]
     public float spreadInsensity;
 
-
-    [Header("Bullet Setting")]
+    [Header("Bullet Settings")]
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
-    public float bulletVelocity = 30;
-    public float bulletPrefabLifeTime = 3f;   // seconds
+    public float bulletVelocity = 30f;
+    public float bulletPrefabLifeTime = 3f;
 
+    public GameObject muzzleEffect;
 
-
-
-    public enum ShootingMode
-    {
-        Single,
-        Burst,
-        Auto
-    }
-
+    public enum ShootingMode { Single, Burst, Auto }
     public ShootingMode currentShootingMode;
 
-    public void Awake()
+    void Awake()
     {
         readyToShoot = true;
         BurstBulletsLeft = bulletsPerBurst;
+        currentAmmo = maxAmmo;
     }
 
-
-    // Update is called once per frame
     void Update()
     {
+        if (isReloading) return;
+
+        // Nhấn R để reload thủ công
+        if (Input.GetKeyDown(KeyCode.R) && currentAmmo < maxAmmo && reserveAmmo > 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        // Xác định trạng thái bắn theo chế độ
         if (currentShootingMode == ShootingMode.Auto)
-        {
             isShooting = Input.GetKey(KeyCode.Mouse0);
-
-
-        }
-        else if (currentShootingMode == ShootingMode.Single || 
-            currentShootingMode == ShootingMode.Burst)
-        {
+        else
             isShooting = Input.GetKeyDown(KeyCode.Mouse0);
-        }
 
-        if (readyToShoot && isShooting)
+        // Bắn nếu đủ đạn
+        if (readyToShoot && isShooting && currentAmmo > 0)
         {
             BurstBulletsLeft = bulletsPerBurst;
             FireWeapon();
         }
 
-
-
-        
-        
+        // Tự reload khi hết đạn
+        if (currentAmmo <= 0 && reserveAmmo > 0 && !isReloading)
+        {
+            StartCoroutine(Reload());
+        }
     }
 
     private void FireWeapon()
     {
+        if (currentAmmo <= 0) return;
 
+        currentAmmo--;
         readyToShoot = false;
 
+        // Muzzle flash
+        if (muzzleEffect != null)
+            muzzleEffect.GetComponent<ParticleSystem>().Play();
+
+        // Tính hướng bắn
         Vector3 shootingDirection = CalculateDirectionAndSpread().normalized;
 
-
-        // Instantiate the bullet
+        // Tạo đạn
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
-        // Poiting the bullet to the shooting direction
         bullet.transform.forward = shootingDirection;
-        // shoot the bullet
         bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
-        // destroy the bullet after time
         StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime));
 
-
+        // Reset bắn
         if (allowReset)
-        {
             Invoke("ResetShot", shootingDelay);
-        }
 
-
-        //Burst mode
+        // Burst mode
         if (currentShootingMode == ShootingMode.Burst && BurstBulletsLeft > 1)
         {
             BurstBulletsLeft--;
             Invoke("FireWeapon", shootingDelay);
         }
-
     }
-
 
     private void ResetShot()
     {
         readyToShoot = true;
         allowReset = true;
     }
-    public Vector3 CalculateDirectionAndSpread()
+
+    private Vector3 CalculateDirectionAndSpread()
     {
         Ray ray = PlayerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         RaycastHit hit;
-
-        Vector3 targetPoint;
-        if (Physics.Raycast(ray, out hit))
-        {
-
-            // hitting sth
-            targetPoint = hit.point;
-        }
-
-        else
-        {
-            // shooting at the air
-            targetPoint = ray.GetPoint(100);
-        }
-
+        Vector3 targetPoint = Physics.Raycast(ray, out hit) ? hit.point : ray.GetPoint(100);
         Vector3 direction = targetPoint - bulletSpawn.position;
 
-        float x = UnityEngine.Random.Range(-spreadInsensity, spreadInsensity);
-        float y = UnityEngine.Random.Range(-spreadInsensity, spreadInsensity);
-
-
-        // Returnning shooting direction and spread
+        float x = Random.Range(-spreadInsensity, spreadInsensity);
+        float y = Random.Range(-spreadInsensity, spreadInsensity);
         return direction + new Vector3(x, y, 0);
-
     }
+
     private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
     {
         yield return new WaitForSeconds(delay);
         Destroy(bullet);
+    }
+
+    private IEnumerator Reload()
+    {
+        isReloading = true;
+        readyToShoot = false;
+
+        if (weaponAnimator != null)
+            weaponAnimator.SetTrigger("ReloadTrigger");
+
+        yield return new WaitForSeconds(reloadTime);
+
+        int ammoNeeded = maxAmmo - currentAmmo;
+        int ammoToLoad = Mathf.Min(ammoNeeded, reserveAmmo);
+        currentAmmo += ammoToLoad;
+        reserveAmmo -= ammoToLoad;
+
+        isReloading = false;
+        readyToShoot = true;
     }
 }
